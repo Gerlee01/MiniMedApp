916fb09755021c14549f1424565935de2bafde62
Gerlee01 <gerleebatzorig25@gmail.com>
2020-11-22 19:21:11 +0800

    Loves

---
+++ lib/entity/user.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class  User {
+  final String userName;
+  final String password;
+
+  User({
+    @required this.userName,
+    @required this.password,
+  });
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import './entity/user.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final User user = User(userName: "gerlee", password: "b@g");
+
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _checkUser() {
+    if (_userNameController.text.isEmpty || _passwordController.text.isEmpty) {
+      return;
+    }
+
+    final inputUserName = _userNameController.text;
+    final inputPassword = _passwordController.text;
+
+    if (inputUserName == user.userName && inputPassword == user.password) {
+      print("Амжилттай нэвтэрлээ");
+    } else {
+      print("Нэвтрэх нэр нууц үг таарсангүй");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mini Med Login"),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: "Нэвтрэх нэр"),
+            controller: _userNameController,
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: "Нууц үг"),
+            controller: _passwordController,
+          ),
+          RaisedButton(
+            child: Text("Нэвтрэх"),
+            onPressed: _checkUser,
+            color: Theme.of(context).primaryColor,
+            textColor: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
