fbe0bebca12f1b4d9d8775b6f994d786dc9002b2
Bayarkhuu <bayrhuu.luya@yahoo.com>
2020-11-23 01:00:57 +0800

    user backend

--- lib/component/user/controller/UserController.dart
+++ lib/component/user/controller/UserController.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart';
 import 'package:mini_med_front/component/user/model/LoginRequest.dart';
 import 'package:mini_med_front/entity/User.dart';
 import 'package:mini_med_front/util/MConstants.dart';
@@ -9,7 +8,8 @@ import 'package:mini_med_front/util/MConstants.dart';
 class UserController {
   get token => null;
 
-  Future<User> findByUsernameAndPassword(String username, String password) async {
+  Future<User> findByUsernameAndPassword(
+      String username, String password) async {
     String url = MConstants.mainService + "/user/me";
     getToken(username, password);
     HttpClient httpClient = HttpClient();
@@ -19,9 +19,9 @@ class UserController {
     if (response.statusCode != 200) {
       return null;
     }
-    Map<String, dynamic> map = json.decode(await response.transform(utf8.decoder).join());
+    Map<String, dynamic> map =
+        json.decode(await response.transform(utf8.decoder).join());
     httpClient.close();
-    print(map);
     return User.fromJson(map);
   }
 
@@ -30,9 +30,11 @@ class UserController {
     HttpClient httpClient = HttpClient();
     HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
     request.headers.set('content-type', 'application/json');
-    request.add(utf8.encode(json.encode(LoginRequest(username: username, password: password).toJson())));
+    request.add(utf8.encode(json.encode(
+        LoginRequest(username: username, password: password).toJson())));
     HttpClientResponse response = await request.close();
-    Map<String, dynamic> map = json.decode(await response.transform(utf8.decoder).join());
+    Map<String, dynamic> map =
+        json.decode(await response.transform(utf8.decoder).join());
     httpClient.close();
     MConstants.setToken(map['accessToken']);
   }
