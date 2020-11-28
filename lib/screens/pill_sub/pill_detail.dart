d801bccc50a8704390aebe6479f0c6c1c2c8e1ab
Gerlee01 <gerleebatzorig25@gmail.com>
2020-11-28 16:29:35 +0800

    эмийн жорын дэлгэрэнгүй загвар засаж дууссан <3

--- lib/screens/pill_sub/pill_detail.dart
+++ lib/screens/pill_sub/pill_detail.dart
@@ -13,64 +13,100 @@ class PillDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(pill.pillName),
       ),
-      body: Column(
-        children: <Widget>[
-          _chooseText(pill.type),
-          Card(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              child: Text(
-                'Жор бичсэн: ${pill.doctorWorkPlace} - ${pill.doctorFullName}',
+      body: Container(
+        color: _chooseColor(pill.type).withOpacity(0.1),
+        padding: const EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: _chooseColor(pill.type),
+                    width: 1,
+                  ),
+                  color: _chooseColor(pill.type),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                child: Text(
+                  'Төрөл: ${_chooseText(pill.type)} \n Жор бичсэн: ${pill.doctorWorkPlace} - ${pill.doctorFullName}',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
               ),
-            ),
+              SizedBox(height: 10),
+              _buildText('Тайлбар', pill.note, context),
+              SizedBox(height: 10),
+              _buildText('Хэрэглэх тун', pill.usedtun , context),
+              SizedBox(height: 10),
+              _buildText('Тун хэтэрсэн үед илрэх шинж, авах арга хэмжээ', pill.usedtunMax , context),
+              SizedBox(height: 10),
+              _buildText('Хэрэглэх заалт', pill.zaalt , context),
+              SizedBox(height: 10),
+              _buildText('Хэрэглэх арга', pill.arga , context),
+              SizedBox(height: 10),
+              _buildText('Жирэмсэн ба хөхүүл үеийн хэрэглээ', pill.pregnantZaalt , context),
+              SizedBox(height: 10),
+              _buildText('Гаж нөлөө', pill.nuloo , context),
+              SizedBox(height: 10),
+              _buildText('Цээрлэлт', pill.tseerlelt , context),
+              SizedBox(height: 10),
+              _buildText('Үйлчлэл', pill.uilchlel , context),
+              SizedBox(height: 10),
+              _buildText('Нэмэлт мэдлэг', pill.nemelt , context),
+              SizedBox(height: 10),
+              _buildText('Олгох нөхцөл', pill.olgoh , context),
+              SizedBox(height: 10),
+              _buildText('Хадгалах нөхцөл', pill.hadgalah , context),
+              SizedBox(height: 10),
+            ],
           ),
-          Container(
-
-            width: double.infinity,
-            child: Text(
-              'Жор:\n ${pill.rp}\n${pill.note}',
-              softWrap: true,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 
-  Widget _chooseText(Type type) {
-    String title = '';
-    Color color = Colors.white;
-
-    switch (type) {
-      case Type.normal:
-        title = 'Энгийн эм';
-        color =Colors.green;
-        break;
-      case Type.setgets:
-        title = 'Сэтгэцэд нөлөөт эм';
-        color =Colors.blueAccent;
-        break;
-      case Type.mansuuruulah:
-        title = 'Мансууруулах эм';
-        color =Colors.red;
-        break;
-      default:
-        title = 'Тодорхойгүй';
-        color =Colors.black12;
-        break;
-    }
-
+  Widget _buildText(String title, String value, BuildContext context){
     return Container(
       width: double.infinity,
-      color: color,
-      padding: EdgeInsets.all(10),
-      child: Text(
-        'Төрөл: $title',
-        style: TextStyle(
-          color: Colors.white,
+      margin: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        border: Border.all(
+          color: Colors.black12,
+          width: 1,
         ),
       ),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        child: Text(title +':\n\n' + value, style: Theme.of(context).textTheme.bodyText2,),
     );
+  }
+
+  Color _chooseColor(Type type) {
+    switch (type) {
+      case Type.normal:
+        return Colors.green;
+      case Type.setgets:
+        return Colors.blueAccent;
+      case Type.mansuuruulah:
+        return Colors.red;
+      default:
+        return Colors.black12;
+    }
+  }
+
+  String _chooseText(Type type) {
+    switch (type) {
+      case Type.normal:
+        return 'Энгийн эм';
+      case Type.setgets:
+        return 'Сэтгэцэд нөлөөт эм';
+      case Type.mansuuruulah:
+        return 'Мансууруулах эм';
+      default:
+        return 'Тодорхойгүй';
+    }
   }
 }
